0fca9c2c180bba3aa284d96cecad82f624aa59fc
Nikifor Bogdanov <nikiforn04@gmail.com>
2024-10-18 16:45:46 +0300

    add qdrant client

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
 from fastapi import FastAPI
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+QDRANT_DOMAIN = os.getenv("QDRANT_DOMAIN")
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+client = QdrantClient(url=QDRANT_DOMAIN)
 
 
 @app.get("/")
