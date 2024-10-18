7e2558ca28e72ca60e62b5f75ebb7d396bd262cd
Nikifor Bogdanov <nikiforn04@gmail.com>
2024-10-19 00:33:15 +0300

    add saving to disk

--- app.py
+++ app.py
@@ -5,23 +5,32 @@ from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams
 from dotenv import load_dotenv
+from langchain_core.documents import Document
+import uuid
 
 load_dotenv()
 
 app = FastAPI()
 
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-QDRANT_DOMAIN = os.getenv("QDRANT_DOMAIN")
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
-client = QdrantClient(":memory:")
-
+client = QdrantClient(path="tmp/langchain-qdrant")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/collection")
+async def get_collection(collection_name: str):
+    vector_store = QdrantVectorStore(
+        client=client,
+        collection_name=collection_name,
+        embedding=embeddings,
+    )
+    return {"message": f"Collection {vector_store.collection_name} retrieved"}
 
 @app.post("/collection")
 async def create_collection(collection_name: str):
@@ -35,3 +44,31 @@ async def create_collection(collection_name: str):
 async def delete_collection(collection_name: str):
     client.delete_collection(collection_name=collection_name)
     return {"message": f"Collection {collection_name} deleted"}
+
+@app.post("/collection/add")
+async def add_to_collection(collection_name: str, text: str):
+    vector_store = QdrantVectorStore(
+        client=client,
+        collection_name=collection_name,
+        embedding=embeddings,
+    )
+    print(text)
+    document_1 = Document(
+    page_content=text,
+    metadata={"source": "user"},
+    )
+    docs = [document_1]
+    ids = [str(uuid.uuid4()) for _ in docs]
+    vector_store.add_documents(documents=docs, ids=ids)
+    return {"message": f"Point added to collection {collection_name}"}
+
+@app.post("/collection/query")  
+async def query_collection(collection_name: str, query: str):
+    vector_store = QdrantVectorStore(
+        client=client,
+        collection_name=collection_name,
+        embedding=embeddings,
+    )
+    docs = vector_store.similarity_search(query)
+    return {"message": f"Query {query} executed", "docs": docs}
+
