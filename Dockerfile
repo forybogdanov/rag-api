2a6a3d999311b109149df033942124504ed0d0a1
Nikifor Bogdanov <nikiforn04@gmail.com>
2024-10-20 21:54:04 +0300

    change pip install command to use no cache

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.12-alpine
 WORKDIR /code
 
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain_core.documents import Document
 import uuid
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
+# TODO: try large model
 model_name = "BAAI/bge-small-en"
 model_kwargs = {"device": "cpu"}
 encode_kwargs = {"normalize_embeddings": True}
